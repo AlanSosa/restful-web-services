4bafb0d0de1d9c6707b5c6514ccd3cfdd5275947
Alan Sosa <alansosame@MacBook-Pro-de-Alan.local>
2020-12-31 16:11:55 -0600

    Adding User Controller

--- src/main/java/com/education/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/education/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,12 @@
 package com.education.rest.webservices.restfulwebservices.user;
 
+import com.sun.jndi.toolkit.url.Uri;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,10 +26,50 @@ public class UserResource {
     private UserDaoService service;
 
     //retrieve all users
+    @GetMapping("/users")
     public List<User> retrieveAllUsers(){
-
+        return service.findAll();
     }
 
     //Retrieve User(int id)
-    //Save a user(
+    @GetMapping("/users/{id}")
+    public User retriveUser(@PathVariable int id){
+        return service.findOne(id);
+    }
+
+
+    /*
+    Jackson automatically maps the json sent in the body to the User bean by validating the name of properties.
+    If you send a property that is not declared in the User bean, jackson will simply ignore it.
+
+    for example:
+    {
+    "name": "Texting name 1",
+    "anotherColumn":"Something here"
+    }
+
+    anotherColumn value will be ignored and jackson will map the bean like this:
+
+    id: null
+    name: "Texting name 1"
+    date : null
+
+     */
+    //Input = details of new User
+    //Output = Created Status & return the created URI (/users/{new id})
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user){
+        User savedUser = service.save(user);
+
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()//Returns the current URI '/users'
+                .path("/{id}") //Then we append the id which is taken from the next method.
+                .buildAndExpand(savedUser.getId()) //This one gets the ID from the User bean.
+                .toUri(); //Parse the whole thing to URI
+
+        /*The location will be in the response headers like this:
+        location → http://localhost:8080/users/5
+         */
+        return ResponseEntity.created(location).build();
+    }
 }
